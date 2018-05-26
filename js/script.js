0c60df4686325d604685b1af9be1e6dc6701244e
zuragogoladze <zuragogoladze@github.com>
2018-05-26 22:46:11 +0400

    added navbar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-    $("#drop").click(function(){
-        $("#drop-down").toggleClass("active");
-    });
+	$("#drop").click(function(){
+		$("#drop-down").toggleClass("active");
+	});
+
+	$('#drop').click(function(){
+		this.css("display", "none");
+	});
 });
